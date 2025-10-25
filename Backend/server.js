ba7821a43c8a8beb752847bf7b89ec7d7de39aa9
banshita rout <banshita.rout.1@gmail.com>
2025-10-26 00:10:43 +0530

    initial

---
+++ Backend/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+const app= express();
+const PORT=8080;
+
+app.use(express.json());
+app.use(cors());
+app.listen(PORT,()=>{
+    console.log(`server running on ${PORT}`);
+});
