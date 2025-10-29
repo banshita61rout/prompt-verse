40b84142c57d2b87f68fe048c85f25dfbf1aaf56
banshita rout <banshita.rout.1@gmail.com>
2025-10-30 01:27:42 +0530

    frontend setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import Sidebar from "./Sidebar";
 import ChatWindow from "./ChatWindow";
+import { Mycontext } from "./Mycontext";
 function App() {
+  const providerValues = {}; //passing values
+
   return (
     <div className="main">
-      <Sidebar></Sidebar>
-      <ChatWindow></ChatWindow>
+      <Mycontext.Provider values={providerValues}>
+        <Sidebar></Sidebar>
+        <ChatWindow></ChatWindow>
+      </Mycontext.Provider>
     </div>
   );
 }
